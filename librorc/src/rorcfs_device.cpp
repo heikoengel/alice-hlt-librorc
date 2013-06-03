98969dce1c869ce6a09e134a9f137f9e234c4477
Heiko Engel <hengel@cern.ch>
2013-06-03 11:30:25 +0200

    corrected pci_ids table for Modelsim SMBus controller binding

--- librorc/src/rorcfs_device.cpp
+++ librorc/src/rorcfs_device.cpp
@@ -55,7 +55,7 @@ rorcfs_device::init
     const char *pci_ids[] =
     {
         "10dc beaf", /* CRORC BETA */
-        "8086 1e22", /* Heikos SMBus Controller for modelsim*/
+        "17aa 1e22", /* Heikos SMBus Controller for modelsim*/
         NULL         /* Delimiter*/
     };
 
