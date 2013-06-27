ae77b1364beb0eb63e72138e70010aa825e99cde
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2013-06-27 14:49:36 +0200

    - removed obsolete loop - adapted coding convention - removed native datatypes

--- apps/src/dma_monitor.cpp
+++ apps/src/dma_monitor.cpp
@@ -52,14 +52,9 @@ int main( int argc, char *argv[] )
     int shID[N_CHANNELS];
     char *shm[N_CHANNELS];
     struct ch_stats *chstats[N_CHANNELS];
-    int i;
 
-    timeval start_time;
-    timeval last_time, cur_time;
-    unsigned long last_bytes_received[N_CHANNELS];
-    unsigned long last_events_received[N_CHANNELS];
-    unsigned long channel_bytes[N_CHANNELS];
-    unsigned long sum_of_bytes, sum_of_bytes_diff;
+    uint64_t sum_of_bytes;
+    uint64_t sum_of_bytes_diff;
 
     /** catch CTRL+C for abort */
     struct sigaction sigIntHandler;
@@ -67,38 +62,40 @@ int main( int argc, char *argv[] )
     sigemptyset(&sigIntHandler.sa_mask);
     sigIntHandler.sa_flags = 0;
 
-    for(i=0; i<N_CHANNELS; i++)
+    /** Innitialize shm channels */
+    uint64_t last_bytes_received[N_CHANNELS];
+    uint64_t last_events_received[N_CHANNELS];
+    uint64_t channel_bytes[N_CHANNELS];
+    for(int32_t i=0;i<N_CHANNELS;i++)
     {
-        shm[i] = NULL;
-        chstats[i] = NULL;
         last_bytes_received[i] = 0;
         last_events_received[i] = 0;
         channel_bytes[i] = 0;
-    }
 
-    for(i=0;i<N_CHANNELS;i++)
-    {
-        shID[i] = shmget(SHM_KEY_OFFSET + i, sizeof(struct ch_stats),
-        IPC_CREAT | 0666);
+        shID[i] = shmget(SHM_KEY_OFFSET + i, sizeof(struct ch_stats), IPC_CREAT | 0666);
         if ( shID[i]==-1)
         {
             perror("shmget");
-            goto out;
+            abort();
         }
 
+        shm[i] = NULL;
         shm[i] = (char *)shmat(shID[i], 0, 0);
         if(shm[i]==(char*)-1)
         {
             perror("shmat");
-            goto out;
+            abort();
         }
+
+        chstats[i] = NULL;
         chstats[i] = (struct ch_stats*)shm[i];
     }
 
     /** capture starting time */
+    timeval start_time;
     gettimeofday(&start_time, 0);
-    last_time = start_time;
-    cur_time = start_time;
+    timeval last_time = start_time;
+    timeval cur_time = start_time;
 
     sigaction(SIGINT, &sigIntHandler, NULL);
 
@@ -110,7 +107,7 @@ int main( int argc, char *argv[] )
         if( gettimeofday_diff(last_time, cur_time) > STAT_INTERVAL )
         {
 
-            for(i=0; i<N_CHANNELS; i++)
+            for(int32_t i=0; i<N_CHANNELS; i++)
             {
                 printf("CH%2d - Events: %10ld, DataSize: %8.3f GB ",
                 i, chstats[i]->n_events,
@@ -154,7 +151,7 @@ int main( int argc, char *argv[] )
 
             sum_of_bytes=0;
             sum_of_bytes_diff=0;
-            for(i=0;i<N_CHANNELS;i++)
+            for(int32_t i=0; i<N_CHANNELS; i++)
             {
                 sum_of_bytes+=chstats[i]->bytes_received;
                 sum_of_bytes_diff+=channel_bytes[i];
@@ -179,9 +176,7 @@ int main( int argc, char *argv[] )
         usleep(100);
     }
 
-
-out:
-    for(i=0;i<N_CHANNELS;i++)
+    for(int32_t i=0; i<N_CHANNELS; i++)
     {
         if (shm[i])
         {
