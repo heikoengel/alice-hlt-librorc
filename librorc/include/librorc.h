bf01af0ed1efdac32dbe72ee5696fe70500c20ba
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2013-07-01 14:05:45 +0200

    - removed obsolete includes

--- librorc/include/librorc.h
+++ librorc/include/librorc.h
@@ -40,15 +40,6 @@
 #ifndef LIBRORC_H
 #define LIBRORC_H
 
-    #include "sim_bar.hh"
-    #include "librorc_registers.h"
-    #include "librorc_defines.hh"
-    #include "librorc_bar_proto.hh"
-    #include "librorc_flash.hh"
-    #include "librorc_sysmon.hh"
-    #include "librorc_bar.hh"
-    #include "librorc_device.hh"
-    #include "librorc_buffer.hh"
-    #include "librorc_dma_channel.hh"
+    #include "include_int.hh"
 
 #endif /** LIBRORC_H */
