a6b7a85991140aeab2b04c5079e884d0336361c8
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2013-07-04 10:58:11 +0200

    - fixed naming bug in prepare script

--- librorc/prepare_build.sh
+++ librorc/prepare_build.sh
@@ -10,15 +10,15 @@ mkdir $BASEDIR/build/sim_debug
 mkdir $BASEDIR/build/sim_release
 
 cd $BASEDIR/build/release
-cmake -DCMAKE_BUILD_TYPE=Release ../../
+cmake -V -DCMAKE_BUILD_TYPE=Release ../../
 
-#cd $BASEDIR/build/debug
-#cmake -DCMAKE_BUILD_TYPE=Debug ../../
+cd $BASEDIR/build/debug
+cmake -DCMAKE_BUILD_TYPE=Debug ../../
 
-#cd $BASEDIR/build/sim
-#cmake -DCMAKE_BUILD_TYPE=Release -DSIM=ON ../../
+cd $BASEDIR/build/sim_release
+cmake -DCMAKE_BUILD_TYPE=Release -DSIM=ON ../../
 
-#cd $BASEDIR/build/sim_debug
-#cmake -DCMAKE_BUILD_TYPE=Debug -DSIM=ON ../../
+cd $BASEDIR/build/sim_debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DSIM=ON ../../
 
 cd $BASEDIR
