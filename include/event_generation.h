5c00ccb3a7e80353437bc6c712c9f5db27f328f0
Heiko Engel <hengel@cern.ch>
2013-08-14 14:06:45 +0200

    pointer bugfix in event_generation.h

--- include/event_generation.h
+++ include/event_generation.h
@@ -19,6 +19,7 @@
  * */
 
 #define MAX_EVENTS_PER_ITERATION 0x0
+#include <math.h>
 
 /**
  * create event
@@ -48,7 +49,7 @@ create_event
 	dest[6] = 0x00000000; // trigger classes high, MBZ, ROI
 	dest[7] = 0xdeadbeaf; // ROI high
 
-	for (i=8; i<length; i++) {
+	for (i=0; i<length-8; i++) {
 		dest[8+i] = i;
 	}
 }
@@ -69,7 +70,7 @@ fill_eventbuffer
     uint64_t buf_space_avail;
     uint32_t fragment_size;
 
-    // get the available event buffer space between the current
+    // get the available event buffer space in bytes between the current
     // generation offset and the last offset written to the channel
     if ( *event_generation_offset < last_eb_offset )
     {
@@ -85,15 +86,16 @@ fill_eventbuffer
     // check how many events can be put into the available space
     // note: EventSize is in DWs and events have to be aligned to
     // MaxReadReq boundaries
+    // fragment_size is in bytes
     uint32_t max_read_req = channel->getPciePacketSize();
     if ( (EventSize<<2) % max_read_req )
     {
         // EventSize is not a multiple of max_read_req
-        fragment_size = (((EventSize<<2) / max_read_req) + 1) * 
+        fragment_size = (trunc((EventSize<<2) / max_read_req) + 1) * 
             max_read_req;
     } else
     {
-        fragment_size = EventSize;
+        fragment_size = (EventSize<<2);
     }
 
     uint64_t nevents = (uint64_t)(buf_space_avail / fragment_size);
@@ -132,7 +134,7 @@ fill_eventbuffer
         // write event data to buffer
         // TODO: new event has to start on a MAX_READ_REQ boundary
         create_event(
-                 eventbuffer + i*fragment_size, // destination pointer
+                 eventbuffer + i*(fragment_size>>2), // destination pointer
                  i, // event ID
                  EventSize ); // event size
 
