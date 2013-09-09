942fd9708f920dce0aa6123cfe52b6974613ccd4
Heiko Engel <hengel@cern.ch>
2013-09-09 12:00:10 +0200

    added CERN PCI Device ID for C-RORC

--- src/device.cpp
+++ src/device.cpp
@@ -37,6 +37,7 @@ device::device
     /** A list of PCI ID to which PDA has to attach. */
     const char *pci_ids[] =
     {
+        "10dc 01a0", /* CRORC as registered at CERN */
         "10dc beaf", /* CRORC BETA */
         "17aa 1e22", /* Heikos SMBus Controller for modelsim*/
         "8086 1e22", /* Heikos SMBus Controller for modelsim*/
