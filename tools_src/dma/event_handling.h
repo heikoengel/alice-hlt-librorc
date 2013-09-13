03fde80e77554a879035a4bd2a458454268f46e5
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2013-09-13 10:04:32 +0200

    - moved functions

--- tools_src/dma/event_handling.h
+++ tools_src/dma/event_handling.h
@@ -8,41 +8,371 @@
 #define PG_PATTERN_RAMP (1<<0)
 
 /** sanity checks **/
-#define CHK_SIZES (1<<0)
+#define CHK_SIZES   (1<<0)
 #define CHK_PATTERN (1<<1)
-#define CHK_SOE (1<<2)
-#define CHK_EOE (1<<3)
-#define CHK_ID (1<<4)
-#define CHK_FILE (1<<8)
-
-/**ABOUT TO BE REMOVED************************************************************/
-//#ifndef EH_LEGACY
-//#define EH_LEGACY
-//
-//    /** Shared mem key offset **/
-//    #define SHM_KEY_OFFSET 2048
-//    /** Shared mem device offset **/
-//    #define SHM_DEV_OFFSET 32
-//
-//    typedef struct
-//    {
-//        uint64_t n_events;
-//        uint64_t bytes_received;
-//        uint64_t min_epi;
-//        uint64_t max_epi;
-//        uint64_t index;
-//        uint64_t set_offset_count;
-//        uint64_t error_count;
-//        int64_t  last_id;
-//        uint32_t channel;
-//    }channelStatus;
-//
-//#endif /** EH_LEGACY */
-/*********************************************************************************/
+#define CHK_SOE     (1<<2)
+#define CHK_EOE     (1<<3)
+#define CHK_ID      (1<<4)
+#define CHK_FILE    (1<<8)
 
 #include <pda.h>
-#include "helper_functions.h"
+#include <librorc.h>
+#include <fcntl.h>
 
+
+/**
+ * TODO: dump to file?
+ * Dump Event
+ * @param eventbuffer pointer to eventbuffer
+ * @param offset offset of the current event within the eventbuffer
+ * @param len size in DWs of the event
+ * */
+void
+dump_event
+(
+    volatile uint32_t *eventbuffer,
+    uint64_t offset,
+    uint64_t len
+)
+{
+#ifdef SIM
+  uint64_t i = 0;
+  for(i=0;i<len;i++) {
+    printf("%03ld: %08x\n",
+        i, (uint32_t)*(eventbuffer + offset +i));
+  }
+  if(len&0x01) {
+    printf("%03ld: %08x (dummy)\n", i,
+        (uint32_t)*(eventbuffer + offset + i));
+    i++;
+  }
+  printf("%03ld: EOE reported_event_size: %08x\n", i,
+      (uint32_t)*(eventbuffer + offset + i));
+#if DMA_MODE==128
+  i++;
+  printf("%03ld: EOE calc_event_size: %08x\n", i,
+      (uint32_t)*(eventbuffer + offset + i));
+  i++;
+  printf("%03ld: EOE dummy %08x\n", i,
+      (uint32_t)*(eventbuffer + offset + i));
+  i++;
+  printf("%03ld: EOE dummy: %08x\n", i,
+      (uint32_t)*(eventbuffer + offset + i));
+#endif
+#endif
+}
+
+
+
+/**
+ * ==========================================================
+ * print summary statistics
+ * ==========================================================
+ * @param n number of channels
+ * @param chstats pointer to struct ch_stats
+ * @param ch_last_bytes_received pointer to array of bytescounts
+ * fromt the last iteration
+ * @param timediff time passed since the last iteration
+ * */
+void
+print_summary_stats
+(
+    uint32_t       n,
+    librorcChannelStatus *chstats[],
+    uint64_t      *ch_last_bytes_received,
+    double         timediff
+)
+{
+  librorcChannelStatus statsum;
+  uint64_t last_bytes_received = 0;
+  uint32_t i;
+  //sum up all channels
+  memset(&statsum, 0, sizeof(statsum));
+  for(i=0;i<n;i++) {
+    statsum.bytes_received += chstats[i]->bytes_received;
+    statsum.error_count += chstats[i]->error_count;
+    statsum.n_events += chstats[i]->n_events;
+    statsum.set_offset_count += chstats[i]->set_offset_count;
+    last_bytes_received += ch_last_bytes_received[i];
+  }
+  // summary stats
+  printf("SUM:  Events: %10ld, DataSize: %8.3f GB ",
+      statsum.n_events,
+      (double)statsum.bytes_received/(double)(1<<30));
+
+  if ( statsum.bytes_received-last_bytes_received)
+  {
+    printf(" DataRate: %9.3f MB/s",
+        (double)(statsum.bytes_received-last_bytes_received)/
+        timediff/(double)(1<<20));
+  } else {
+    printf(" DataRate: -");
+  }
+  printf(" Errors: %ld\n", statsum.error_count);
+}
+
+
+
+/**
+ * Dump reportbuffer entry
+ * @param reportbuffer pointer to reportbuffer
+ * @param i index of current struct librorc_event_descriptor within
+ * reportbuffer
+ * @param ch DMA channel number
+ * */
+void
+dump_rb
+(
+    struct librorc_event_descriptor *reportbuffer,
+    uint64_t i,
+    uint32_t ch
+)
+{
+    DEBUG_PRINTF(PDADEBUG_CONTROL_FLOW,
+            "CH%2d - RB[%3ld]: calc_size=%08x\t"
+            "reported_size=%08x\t"
+            "offset=%lx\n",
+            ch, i, reportbuffer->calc_event_size,
+            reportbuffer->reported_event_size,
+            reportbuffer->offset);
+}
+
+
+//TODO: this looks like redundant with the dma monitor
+/**
+ * ==========================================================
+ * print channel statistics
+ * ==========================================================
+ * @param n number of channels
+ * @param chstats pointer to struct ch_stats
+ * @param ch_last_bytes_received pointer to array of bytescounts
+ * fromt the last iteration
+ * @param timediff time passed since the last iteration
+ * */
+void
+print_channel_stats
+(
+    uint32_t n,
+    librorcChannelStatus *chstats[],
+    uint64_t *ch_last_bytes_received,
+    double timediff
+)
+{
+  librorcChannelStatus statsum;
+  uint64_t last_bytes_received = 0;
+  uint32_t i;
+  //sum up all channels
+  memset(&statsum, 0, sizeof(statsum));
+  for(i=0;i<n;i++) {
+    statsum.bytes_received += chstats[i]->bytes_received;
+    statsum.error_count += chstats[i]->error_count;
+    statsum.n_events += chstats[i]->n_events;
+    statsum.set_offset_count += chstats[i]->set_offset_count;
+    last_bytes_received += ch_last_bytes_received[i];
+  }
+
+  // summary stats
+  printf("\n********Overall********\n");
+  print_summary_stats(n, chstats, ch_last_bytes_received, timediff);
+  printf("-------------------------\n");
+
+  // per channel stats
+  for(i=0;i<n;i++) {
+    printf("CH%2d: Events: %10ld, DataSize: %8.3f GB ",
+        i, chstats[i]->n_events,
+        (double)chstats[i]->bytes_received/(double)(1<<30));
+
+    if ( chstats[i]->bytes_received-ch_last_bytes_received[i])
+    {
+      printf(" DataRate: %9.3f MB/s",
+          (double)(chstats[i]->bytes_received-
+            ch_last_bytes_received[i])/
+          timediff/(double)(1<<20));
+    } else {
+      printf(" DataRate: -");
+    }
+    printf(" Errors: %ld\n", chstats[i]->error_count);
+    ch_last_bytes_received[i] = chstats[i]->bytes_received;
+  }
+}
+
+
+//TODO: put this into the dma channel class!
+void
+dump_diu_state
+(
+    librorc::dma_channel *ch
+)
+{
+  uint32_t status;
+  status = ch->getGTX(RORC_REG_DDL_CTRL);
+  printf("\nDIU_IF: ");
+  if ( status & 1 ) printf("DIU_ON ");
+  else printf("DIU_OFF ");
+  if ( status>>1 & 1) printf("FC_ON ");
+  else printf("FC_OFF ");
+  if ( !(status>>4 & 1) ) printf("LF ");
+  if ( !(status>>5 & 1) ) printf("LD ");
+  if ( !(status>>30 & 1) ) printf("BSY ");
+  if ( status>>8 & 1) printf("PG_ON");
+  else printf("PG_OFF");
+
+  if ( !(status>>8 & 1) ) { // PG disabled
+    printf("CTSTW:%08x ", ch->getGTX(RORC_REG_DDL_CTSTW));
+    printf("DEADTIME:%08x ", ch->getGTX(RORC_REG_DDL_DEADTIME));
+    printf("EC:%08x ", ch->getGTX(RORC_REG_DDL_EC));
+  }
+}
+
+
+
+/**
+ * dump event to file(s)
+ * @param base_dir pointer to destination directory string
+ * @param pointer to stats channel stats struct
+ * @param file_index index of according file, appears in file name
+ * @param reportbuffer pointer to reportbuffer
+ * @param ebuf pointer to librorc::buffer
+ * @return 0 on sucess, -1 on error
+ * */
+int dump_to_file
+(
+    char            *base_dir,
+    librorcChannelStatus   *stats,
+    uint64_t        EventID,
+    uint32_t         file_index,
+    struct librorc_event_descriptor *reportbuffer,
+    librorc::buffer *ebuf,
+    uint32_t        error_flags
+)
+{
+  char *ddlname = NULL;
+  char *logname = NULL;
+  int len, result;
+  FILE *fd_ddl, *fd_log;
+  uint32_t i;
+  uint32_t *eventbuffer = (uint32_t *)ebuf->getMem();
+
+  // get length of destination file string
+  len = snprintf(NULL, 0, "%s/ch%d_%d.ddl",
+      base_dir, stats->channel, file_index);
+  if (len<0) {
+    perror("dump_to_file::snprintf failed");
+    return -1;
+  }
+
+  // allocate memory for destination file string
+  ddlname = (char *)malloc(len+1);
+  if (ddlname==NULL) {
+    perror("dump_to_file::malloc(ddlname) failed");
+    return -1;
+  }
+  logname = (char *)malloc(len+1);
+  if (logname==NULL) {
+    perror("dump_to_file::malloc(logname) failed");
+    return -1;
+  }
+
+  // fill destination file string
+  snprintf(ddlname, len+1, "%s/ch%d_%d.ddl",
+      base_dir, stats->channel, file_index);
+  snprintf(logname, len+1, "%s/ch%d_%d.log",
+      base_dir, stats->channel, file_index);
+
+  // open DDL file
+  fd_ddl = fopen(ddlname, "w");
+  if ( fd_ddl < 0 ) {
+    perror("failed to open destination DDL file");
+    return -1;
+  }
+
+  // open log file
+  fd_log =  fopen(logname, "w");
+  if ( fd_ddl == NULL )
+  {
+    printf("failed to open destination LOG file : %s\n", ddlname);
+    return -1;
+  }
+
+  // dump RB entry to log
+  fprintf(fd_log, "CH%2d - RB[%3ld]: \ncalc_size=%08x\n"
+      "reported_size=%08x\n"
+      "offset=%lx\n"
+      "EventID=%lx\n"
+      "LastID=%lx\n",
+      stats->channel, stats->index,
+      reportbuffer[stats->index].calc_event_size,
+      reportbuffer[stats->index].reported_event_size,
+      reportbuffer[stats->index].offset,
+      EventID,
+      stats->last_id);
+
+  /** dump error type */
+  fprintf(fd_log, "Check Failed: ");
+  if ( error_flags & CHK_SIZES ) fprintf(fd_log, " CHK_SIZES");
+  if ( error_flags & CHK_PATTERN ) fprintf(fd_log, " CHK_PATTERN");
+  if ( error_flags & CHK_SOE ) fprintf(fd_log, " CHK_SOE");
+  if ( error_flags & CHK_EOE ) fprintf(fd_log, " CHK_EOE");
+  if ( error_flags & CHK_ID ) fprintf(fd_log, " CHK_ID");
+  if ( error_flags & CHK_FILE ) fprintf(fd_log, " CHK_FILE");
+  fprintf(fd_log, "\n\n");
+
+  // check for reasonable calculated event size
+  if (reportbuffer[stats->index].calc_event_size > (ebuf->getPhysicalSize()>>2))
+  {
+    fprintf(fd_log, "calc_event_size (0x%x DWs) is larger than physical "
+        "buffer size (0x%lx DWs) - not dumping event.\n",
+        reportbuffer[stats->index].calc_event_size,
+        (ebuf->getPhysicalSize()>>2) );
+  }
+  // check for reasonable offset
+  else if (reportbuffer[stats->index].offset > ebuf->getPhysicalSize())
+  {
+    fprintf(fd_log, "offset (0x%lx) is larger than physical "
+        "buffer size (0x%lx) - not dumping event.\n",
+        reportbuffer[stats->index].offset,
+        ebuf->getPhysicalSize() );
+  }
+  else
+  {
+    // dump event to log
+    for(i=0;i<reportbuffer[stats->index].calc_event_size;i++)
+    {
+        uint32_t ebword = (uint32_t)*(eventbuffer +
+                (reportbuffer[stats->index].offset>>2) + i);
+
+        fprintf(fd_log, "%03d: %08x", i, ebword);
+        if ( (error_flags & CHK_PATTERN) &&
+                (i>7) && (ebword != i-8) )
+        {
+            fprintf(fd_log, " expected %08x", i-8);
+        }
+
+        fprintf(fd_log, "\n");
+    }
+
+    fprintf(fd_log, "%03d: EOE reported_event_size: %08x\n", i,
+        (uint32_t)*(eventbuffer + (reportbuffer[stats->index].offset>>2) + i));
+
+    //dump event to DDL file
+    result = fwrite(eventbuffer + (reportbuffer[stats->index].offset>>2), 4,
+        reportbuffer[stats->index].calc_event_size, fd_ddl);
+    if( result<0 ) {
+      perror("failed to copy event data into DDL file");
+      return -1;
+    }
+
+  }
+
+  fclose(fd_log);
+  fclose(fd_ddl);
+
+  return 0;
+}
+
+
+
+//TODO : this going to be refactored into a class
 /**
  * Sanity checks on received data
  * @param reportbuffer pointer to struct librorc_event_descriptor
@@ -155,7 +485,6 @@ int event_sanity_check
     }
   }
 
-
   // compare with reference DDL file
   if ( check_mask & CHK_FILE )
   {
@@ -171,8 +500,6 @@ int event_sanity_check
         retval |= CHK_FILE;
     }
 
-
-
     for (j=0;j<calc_event_size;j++) {
         if ( eb[j] != ddlref[j] ) {
             DEBUG_PRINTF(PDADEBUG_ERROR,
@@ -184,9 +511,6 @@ int event_sanity_check
       }
     }
   }
-
-
-
 
   if ( check_mask & CHK_EOE )
   {
@@ -210,7 +534,6 @@ int event_sanity_check
       }
 
   } // CHK_EOE
-
 
   // get EventID from CDH:
   // lower 12 bits in CHD[1][11:0]
@@ -241,160 +564,6 @@ int event_sanity_check
       free(eb);
   }
   return retval;
-}
-
-
-
-
-/**
- * handle incoming data
- *
- * check if there is a reportbuffer entry at the current polling index
- * if yes, handle all available reportbuffer entries
- * @param rbuf pointer to ReportBuffer
- * @param eventbuffer pointer to EventBuffer Memory
- * @param channel pointer
- * @param ch_stats pointer to channel stats struct
- * @param do_sanity_check mask of sanity checks to be done on the
- * received data. See CHK_* defines above.
- * @return number of events processed
- **/
-int handle_channel_data
-(
-    librorc::buffer      *rbuf,
-    librorc::buffer      *ebuf,
-    librorc::dma_channel *channel,
-    librorcChannelStatus *stats,
-    int                   do_sanity_check,
-    uint32_t             *ddlref,
-    uint64_t              ddlref_size
-)
-{
-    uint64_t events_per_iteration = 0;
-    int events_processed = 0;
-    uint64_t eboffset = 0;
-    uint64_t rboffset = 0;
-    uint64_t starting_index, entrysize;
-    struct librorc_event_descriptor rb;
-    uint64_t EventID;
-    char    basedir[] = "/tmp";
-    int retval;
-
-    struct librorc_event_descriptor *reportbuffer =
-    (struct librorc_event_descriptor *)rbuf->getMem();
-    volatile uint32_t *eventbuffer = (uint32_t *)ebuf->getMem();
-
-    // new event received
-    if( reportbuffer[stats->index].calc_event_size!=0 )
-    {
-        // capture index of the first found reportbuffer entry
-        starting_index = stats->index;
-
-        // handle all following entries
-        while( reportbuffer[stats->index].calc_event_size!=0 )
-        {
-            // increment number of events processed in this interation
-            events_processed++;
-
-            // perform validity tests on the received data (if enabled)
-            if (do_sanity_check)
-            {
-                rb = reportbuffer[stats->index];
-                retval = event_sanity_check(
-                &rb,
-                eventbuffer,
-                stats->index,
-                stats->channel,
-                stats->last_id,
-                PG_PATTERN_RAMP,
-                do_sanity_check,
-                ddlref,
-                ddlref_size,
-                &EventID);
-
-
-                if ( retval!=0 )
-                {
-                stats->error_count++;
-
-                    if (stats->error_count < MAX_FILES_TO_DISK)
-                    {
-                        dump_to_file(
-                        basedir, // base dir
-                        stats, // channel stats
-                        EventID, // current EventID
-                        stats->error_count, // file index
-                        reportbuffer, // Report Buffer
-                        ebuf, // Event Buffer
-                        retval // Error flags
-                        );
-                    }
-                }
-
-                stats->last_id = EventID;
-            }
-
-            #ifdef DEBUG
-            dump_rb(&reportbuffer[stats->index], stats->index, stats->channel);
-            #endif
-
-            // increment the number of bytes received
-            stats->bytes_received +=
-                (reportbuffer[stats->index].calc_event_size<<2);
-
-            // save new EBOffset
-            eboffset = reportbuffer[stats->index].offset;
-
-            // increment reportbuffer offset
-            rboffset =
-                ((stats->index)*sizeof(struct librorc_event_descriptor))
-                    % rbuf->getPhysicalSize();
-
-            // wrap RB index if necessary
-            if( stats->index < rbuf->getMaxRBEntries()-1 )
-            {
-                stats->index++;
-            }
-            else
-            {
-                stats->index=0;
-            }
-
-            //increment total number of events received
-            stats->n_events++;
-
-            //increment number of events processed in this while-loop
-            events_per_iteration++;
-        }
-
-        // clear processed reportbuffer entries
-        entrysize = sizeof(struct librorc_event_descriptor);
-        memset(&reportbuffer[starting_index], 0,
-        events_per_iteration*entrysize);
-
-
-        // update min/max statistics on how many events have been received
-        // in the above while-loop
-        if(events_per_iteration > stats->max_epi)
-        {
-            stats->max_epi = events_per_iteration;
-        }
-
-        if(events_per_iteration < stats->min_epi)
-        {
-            stats->min_epi = events_per_iteration;
-        }
-        events_per_iteration = 0;
-        stats->set_offset_count++;
-
-        channel->setBufferOffsetsOnDevice(eboffset, rboffset);
-
-        DEBUG_PRINTF(PDADEBUG_CONTROL_FLOW,
-        "CH %d - Setting swptrs: RBDM=%016lx EBDM=%016lx\n",
-        stats->channel, rboffset, eboffset);
-    }
-
-    return events_processed;
 }
 
 #endif
