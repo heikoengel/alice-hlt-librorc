ffd927dcb6902aa846a6e64580f83b14bc138af3
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2013-09-16 21:49:52 +0200

    - test

--- tools_src/dma/dma_in.cpp
+++ tools_src/dma/dma_in.cpp
@@ -77,9 +77,9 @@ int event_sanity_check
 //
 //    try
 //    {
-//        *event_id
-//            = checker.check
-//              (reportbuffer, i, last_id);
+        *event_id
+            = checker.check
+              (reportbuffer, i, last_id);
 //    }
 //    catch( int error )
 //    {
