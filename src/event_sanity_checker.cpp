758d6e9807a4e4e8cc4e322a1ebb66540c6b6d7c
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2013-09-16 22:05:44 +0200

    - stuff

--- src/event_sanity_checker.cpp
+++ src/event_sanity_checker.cpp
@@ -44,7 +44,7 @@ event_sanity_checker::check
            reportedEventSize(report_buffer)
           );
 
-//    retval |= !(m_check_mask & CHK_SIZES)   ? 0 : compareCalculatedToReportedEventSizes(report_buffer, report_buffer_index);
+    retval |= !(m_check_mask & CHK_SIZES)   ? 0 : compareCalculatedToReportedEventSizes(report_buffer, report_buffer_index);
 //    retval |= !(m_check_mask & CHK_SOE)     ? 0 : checkStartOfEvent(report_buffer, report_buffer_index);
 //    retval |= !(m_check_mask & CHK_PATTERN) ? 0 : checkPattern(report_buffer, report_buffer_index);
 //    retval |= !(m_check_mask & CHK_FILE)    ? 0 : compareWithReferenceDdlFile(report_buffer, report_buffer_index);
