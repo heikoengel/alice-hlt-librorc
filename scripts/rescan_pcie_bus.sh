ea30fedce11d2eb6aeb4fc40a4db202776b1754e
Heiko Engel <hengel@cern.ch>
2013-12-20 12:11:16 +0100

    rescan script update for HAPRO PC

--- scripts/rescan_pcie_bus.sh
+++ scripts/rescan_pcie_bus.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-PCI_IDS[0]="0000\:02\:04.0"
-PCI_IDS[1]="0000\:06\:00.0"
-PCI_IDS[2]="0000\:09\:04.0"
-PCI_IDS[3]="0000\:0c\:04.0"
-PCI_IDS[4]="0000\:0f\:04.0"
+PCI_IDS[0]=0000\:01\:00.0
+PCI_IDS[1]=0000\:02\:00.0
+PCI_IDS[2]=0000\:04\:00.0
+PCI_IDS[3]=0000\:05\:00.0
+PCI_IDS[4]=0000\:06\:00.0
+PCI_IDS[4]=0000\:08\:00.0
 
 for ID in ${PCI_IDS[@]}
 do
   if [ -f /sys/bus/pci/devices/${ID}/remove ]
   then
+    echo "Removing ${ID}"
     echo 1 > /sys/bus/pci/devices/${ID}/remove
   fi
 done
 
 # rescan PCIe bus
+echo "Rescanning..."
 echo 1 > /sys/bus/pci/rescan
 
 # make sure uio_pci_dma is loaded
@@ -27,6 +30,11 @@ fi
 echo "10dc 01a0" >> /sys/bus/pci/drivers/uio_pci_dma/new_id
 
 # chmod a+rw for all detected devices
-find -L /sys/module/uio_pci_dma/drivers/pci\:uio_pci_dma/ -maxdepth 2 -iname "*resource1*" -exec chmod a+rw {} \; 2>&1 | sed -n 's/.*\([0-9:\.]\{12\}\+\).*/found device at \1/p'
+find -L /sys/module/uio_pci_dma/drivers/pci\:uio_pci_dma/ -maxdepth 2 -iname "*resource1*" -exec chmod a+rw {} \; 2>&1 | sed -n 's/.*\([0-9:\.]\{12\}\+\).*/Found device at \1/p'
 
+# make sure i2c-dev is loaded
+if [ ! -d /sys/module/i2c_dev ]
+then
+  modprobe i2c_dev
+fi
 chmod a+rw /dev/i2c-*
