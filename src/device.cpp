7dd33afff3d9197f42dac98bc1c65c2cd78f018d
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2014-01-09 16:11:19 +0100

    - added skeleton for in and out checking

--- src/device.cpp
+++ src/device.cpp
@@ -56,6 +56,39 @@ device::device
     if(DeviceOperator_getPciDevice(m_dop, &m_device, device_index) != PDA_SUCCESS)
     { throw LIBRORC_DEVICE_ERROR_CONSTRUCTOR_FAILED; }
 
+    #ifndef SIM
+        Bar *bar;
+        if(PciDevice_getBar(m_device, &bar, 1) != PDA_SUCCESS)
+        { throw LIBRORC_DEVICE_ERROR_CONSTRUCTOR_FAILED; }
+
+        uint8_t *buffer = NULL;
+        uint64_t length = 0;
+        if(Bar_getMap(bar, &buffer, &length) != PDA_SUCCESS)
+        { throw LIBRORC_DEVICE_ERROR_CONSTRUCTOR_FAILED; }
+
+        switch (esType)
+        {
+            case LIBRORC_ES_IN_GENERIC:
+            case LIBRORC_ES_IN_DDL:
+            case LIBRORC_ES_IN_HWPG:
+            {
+                //dma_direction = LIBRORC_DMA_FROM_DEVICE;
+            }
+            break;
+
+            case LIBRORC_ES_OUT_GENERIC:
+            case LIBRORC_ES_OUT_SWPG:
+            case LIBRORC_ES_OUT_FILE:
+            {
+                //dma_direction = LIBRORC_DMA_TO_DEVICE;
+            }
+            break;
+            default:
+            { throw LIBRORC_DEVICE_ERROR_CONSTRUCTOR_FAILED; }
+        }
+
+    #endif
+
     m_number = device_index;
 }
 
