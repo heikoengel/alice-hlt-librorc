14abdedd277cb5695ada25a5dcbe3b15824ccc4a
Dominic Eschweiler <eschweiler@fias.uni-frankfurt.de>
2014-03-14 08:48:07 +0100

    - added installation remarks to README

--- scripts/buffer.sh
+++ scripts/buffer.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 PROG="../build/release/tools_src/bufferstats"
+FILE=sg_entries.csv
 
-#../build/release/tools_src/crorc_preallocator
+../build/release/tools_src/crorc_preallocator >> /dev/null
 
 for I in {0..23}
 do
-    echo -n `$PROG -n 0 -b 0 | grep "Size:" | awk '{print $4}' | cut -d"(" -f2`
-    echo -n " ; "
-    $PROG -n 0 -b $I | grep "SG Entries" | awk '{print $5}'
+    echo -n `$PROG -n 0 -b $I | grep "Size:" | awk '{print $4}' | cut -d"(" -f2` >> $FILE
+    echo -n " ; "                                                                >> $FILE
+    $PROG -n 0 -b $I | grep "SG Entries" | awk '{print $5}'                      >> $FILE
 done
