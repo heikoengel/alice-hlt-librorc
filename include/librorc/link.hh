heikoengel/alice-hlt-librorc

commit 25c9292953e6c72b5487f3b2a741bbccf6878d06

refactorings in link class: removing obsolete/duplicate code; updates on parts of board_reset script (untested)

include/librorc/link.hh
@@ -137,7 +137,7 @@ namespace LIBRARY_NAME
 
             /**
              * get GTX clock domain status
-             * @return TRUE is up and running, FALSE if down
+             * @return TRUE if up and running, FALSE if down
              * */
             bool isGtxDomainReady();
 
@@ -283,52 +283,46 @@ namespace LIBRARY_NAME
 
 
             /**
-             * Enable Flow Control
+             * Enable/disable Flow Control
              * With flow control enabled the datapath respects full FIFOs
              * and throttles the data flow from source. No XOFF is sent
              * to DAQ as long as this is not enabled.
+             * With flow control enabled the datapath from source
+             * to DMA engine ignores any full FIFOs and will never
+             * send XOFF to DAQ. This also means that DMA'ed events
+             * may be incomplete.
+             * @param enable 1 to enable, 0 to disable
              **/
             void
-            enableFlowControl();
-
-            /**
-             * Disable Flow Control
-             * The datapath from source to DMA engine ignores any full
-             * FIFOs and will never send XOFF to DAQ. This also means
-             * that DMA'ed events may be incomplete.
-             **/
-            void
-            disableFlowControl();
-
-
-            /**********************************************************
-             *             Fast Cluster Finder Interfacing
-             *********************************************************/
-
-            /**
-             * load mapping file into FCF mapping RAM
-             * @param fname path to mapping file. The mapping file
-             *        is a plain-text file with one 32bit hex string
-             *        per line, e.g.
-             *        "0x1000801f"
-             **/
-            void
-            fcfLoadMappingRam
+            setFlowControlEnable
             (
-                 const char *fname
+                uint32_t enable
             );
 
             /**
-             * enable FastClusterFinder processing
+             * check if Flow Control is enabled
+             * @return true if enabled, false if disabled
              **/
-            void
-            enableFcf();
+            bool
+            flowControlIsEnabled();
+
 
             /**
-             * disable FastClusterFinder processing
+             * check if current firmware has a pattern genertor
+             * implemented
+             * @return true if available, else false
              **/
-            void
-            disableFcf();
+            bool
+            patternGeneratorAvailable();
+
+
+            /**
+             * check if current firmware has DDR3 Data Replay
+             * implemented for current link.
+             * @return true if available, else false
+             **/
+            bool
+            ddr3DataReplayAvailable();
 
 
 
@@ -434,12 +428,6 @@ namespace LIBRARY_NAME
             bool
             ddr3DataReplayChannelIsDone();
 
-            /**
-             * check link status on GTX level
-             **/
-            bool
-            gtxIsUp();
-
 
         protected:
             bar      *m_bar;
@@ -481,35 +469,6 @@ namespace LIBRARY_NAME
                 uint8_t addr,
                 uint8_t value
             );
-
-
-
-            /**********************************************************
-             *             Fast Cluster Finder Interfacing
-             *********************************************************/
-
-            /** write entry into FCF mapping RAM
-             * @param addr mapping RAM addr
-             * @param data data to be written
-             **/
-            /*void
-            fcfWriteMappingRamEntry
-            (
-                 uint32_t addr,
-                 uint32_t data
-            );*/
-
-            /**
-             * convert hex string to uint32_t
-             * @param line input string
-             * @return line as uint32_t
-             **/
-            /*uint32_t
-            fcfHexstringToUint32
-            (
-                 std::string line
-            );*/
-
 };
 
 }

src/link.cpp
@@ -192,8 +192,20 @@ namespace LIBRARY_NAME
     bool
     link::isGtxDomainReady()
     {
-        uint32_t gtxasynccfg = packetizer(RORC_REG_GTX_ASYNC_CFG);
-        return( (gtxasynccfg & 0x00000134) == 0x00000034 );
+        uint32_t gtxsyncsts = GTX(RORC_REG_GTX_CTRL);
+        uint32_t stsmask = (1<<0) | // PHY is ready
+            (1<<1) | // PHY is enabled enable
+            (1<<5) | // GTX byteisaligned
+            (1<<6) | // GTX lossofsync
+            (1<<9) | // GTX rxbufstatus[2]
+            (1<<11); // GTX txbufstatus[1]
+        uint32_t expected_value = (1<<0) | // ready==1
+            (1<<1) | // enable==1
+            (1<<5) | // byteisaligned==1
+            (0<<6) | // lossofsync==0
+            (0<<9) | // rxbufstatus[2]==0
+            (0<<11); // txbufstatus[1]==0
+        return ((gtxsyncsts & stsmask) == expected_value);
     }
 
     void
@@ -526,77 +538,51 @@ namespace LIBRARY_NAME
     link::waitForGTXDomain()
     {
         /**
-         * poll asynchronous GTX status
-         * wait for rxresetdone & txresetdone & rxplllkdet &
-         * txplllkdet & !gtx_in_rst
-         *
          * TODO:
          * - add timeout
          * - return/handle/report timeout error
          * */
-        while( (packetizer(RORC_REG_GTX_ASYNC_CFG) & 0x134) != 0x034 )
+        while( !isGtxDomainReady() )
         { usleep(100); }
     }
 
 
-    /**********************************************************
-     *             Fast Cluster Finder Interfacing
-     * *******************************************************/
-
-    // protected
-    /*void
-    link::fcfWriteMappingRamEntry
+    void
+    link::setFlowControlEnable
     (
-        uint32_t addr,
-        uint32_t data
+        uint32_t enable
     )
     {
-        setGTX(RORC_REG_FCF_RAM_DATA, data);
-        setGTX(RORC_REG_FCF_RAM_CTRL, addr);
-    }*/
-
-
-    // protected
-
-
-    void
-    link::enableFlowControl()
-    {
         uint32_t ddlctrl = GTX(RORC_REG_DDL_CTRL);
-        ddlctrl |= (1<<1); // enable flow control
+        ddlctrl &= ~(1<<1); // clear bit
+        ddlctrl |= ((enable & 1)<<1); // set new value
         setGTX(RORC_REG_DDL_CTRL, ddlctrl);
     }
 
-    void
-    link::disableFlowControl()
+    bool
+    link::flowControlIsEnabled()
     {
-        uint32_t ddlctrl = GTX(RORC_REG_DDL_CTRL);
-        ddlctrl &= ~(1<<1); // disable flow control
-        setGTX(RORC_REG_DDL_CTRL, ddlctrl);
+        return( (GTX(RORC_REG_DDL_CTRL) & (1<<1)) != 0 );
     }
 
-    void
-    link::enableFcf()
+
+    bool
+    link::patternGeneratorAvailable()
     {
-        // enable FCF (same bit as PG_ENABLE)
-        uint32_t ddlctrl = GTX(RORC_REG_DDL_CTRL);
-        ddlctrl |= (1<<8); // enable PatternGenerator
-        setGTX(RORC_REG_DDL_CTRL, ddlctrl);
+        return( (GTX(RORC_REG_DDL_CTRL) & (1<<15)) != 0 );
     }
 
-    void
-    link::disableFcf()
+
+    bool
+    link::ddr3DataReplayAvailable()
     {
-        uint32_t ddlctrl = GTX(RORC_REG_DDL_CTRL);
-        ddlctrl &= ~(1<<8); // disable FCF (same bit as PG_ENABLE)
-        setGTX(RORC_REG_DDL_CTRL, ddlctrl);
+        return( (GTX(RORC_REG_DDL_CTRL) & (1<<24)) != 0 );
     }
 
     void
     link::setDataSourceDdr3DataReplay()
     {
         // set MUX to DDR3 DataReplay:
-        // same as PatternGenerator for HLT_IN & HWTEST
         uint32_t ddlctrl = GTX(RORC_REG_DDL_CTRL);
         ddlctrl &= ~(3<<16);
         ddlctrl |= (1<<16); // set MUX to 1
@@ -725,29 +711,5 @@ namespace LIBRARY_NAME
     link::ddr3DataReplayChannelIsDone()
     {
         return ((GTX(RORC_REG_DDR3_DATA_REPLAY_CHANNEL_STS) & (1<<0)) != 0);
-    }
-
-    bool
-    link::gtxIsUp()
-    {
-        /**
-         * TODO: add checks for
-         * - dfe eye opening?
-         * - error counters?
-         **/
-        uint32_t gtxsyncsts = GTX(RORC_REG_GTX_CTRL);
-        uint32_t stsmask = (1<<0) | // ready
-            (1<<1) | // enable
-            (1<<5) | // byteisaligned
-            (1<<6) | // lossofsync
-            (1<<9) | // rxbufstatus[2]
-            (1<<11); // txbufstatus[1]
-        uint32_t expected_value = (1<<0) | // ready==1
-            (1<<1) | // enable==1
-            (1<<5) | // byteisaligned==1
-            (0<<6) | // lossofsync==0
-            (0<<9) | // rxbufstatus[2]==0
-            (0<<11); // txbufstatus[1]==0
-        return ((gtxsyncsts & stsmask) == expected_value);
     }
 }

tools_src/dma/dma_in_fcf.cpp
@@ -221,7 +221,7 @@ int main(int argc, char *argv[])
     //        (link->packetizer(RORC_REG_DMA_CTRL) | 0x0d) );
 
     link->setDataSourceDdr3DataReplay();
-    link->enableFlowControl();
+    link->setFlowControlEnable(1);
     link->setDdr3DataReplayChannelReset(0);
     link->setDdr3DataReplayChannelContinuous(0);
     link->setDdr3DataReplayChannelOneshot(0);

src/diu.cpp
@@ -117,7 +117,7 @@ namespace LIBRARY_NAME
         else
         {
             /** check link state before sending commands */
-            if( !m_link->gtxIsUp() )
+            if( !m_link->isGtxDomainReady() )
             {
                 DEBUG_PRINTF(PDADEBUG_ERROR, "Unexpected GTX state -"
                         " will not send DIU commands!\n");

tools_src/dma/dma_handling.cpp
@@ -549,7 +549,7 @@ configureDataSource
 )
 {
     eventStream->m_link->waitForGTXDomain();
-    eventStream->m_link->enableFlowControl();
+    eventStream->m_link->setFlowControlEnable(1);
     if( opts.esType==LIBRORC_ES_TO_DEVICE &&
             opts.datasource != ES_SRC_NONE)
     {

tools_src/linkctrl/ddlctrl.cpp
@@ -353,7 +353,7 @@ int main
             if ( pgnevents )
             {
                 ddlctrl &= ~(1<<10); //clear continuous-bit
-            } 
+            }
             else
             {
                 ddlctrl |= (1<<10); //set continuous-bit
@@ -416,10 +416,8 @@ int main
         /** set flow control */
         if ( set_fc )
         {
-            if(fc)
-            { current_link->enableFlowControl(); }
-            else
-            { current_link->disableFlowControl(); }
+            ddlctrl &= ~(1<<1); // clear flow control bit
+            ddlctrl |= ((fc&1)<<1);
         }
 
         if ( set_fc || set_enable || set_pgmode || set_pgenable ||

tools_src/rorctl/board_reset.cpp
@@ -124,9 +124,13 @@ main
         channel_number : 0;
     uint32_t end_channel = sm->numberOfChannels()-1;
 
+    uint32_t firmware_type = sm->firmwareType();
+
     for ( uint32_t i=start_channel; i<=end_channel; i++ )
     {
         librorc::link *link = new librorc::link(bar, i);
+        uint32_t link_type = link->linkType();
+
 
         if ( do_full_reset )
         {
@@ -141,62 +145,70 @@ main
 
         if(link->isGtxDomainReady())
         {
+            link->setFlowControlEnable(0);
             link->clearAllGtxErrorCounters();
 
-            switch( sm->firmwareType() )
+            switch( link_type )
             {
-                case RORC_CFG_PROJECT_hlt_in:
+                case RORC_CFG_LINK_TYPE_DIU:
                     {
                         librorc::diu *diu = new librorc::diu(link);
+                        diu->setReset(1);
                         diu->disableInterface();
+                        diu->clearEventcount();
+                        diu->clearDeadtime();
                         diu->clearAllLastStatusWords();
                         delete diu;
 
-                        librorc::patterngenerator *pg =
-                            new librorc::patterngenerator(link);
-                        pg->disable();
-                        delete pg;
-                    }
-                    break;
-                case RORC_CFG_PROJECT_hwtest:
-                    {
-                        librorc::patterngenerator *pg =
-                            new librorc::patterngenerator(link);
-                        pg->disable();
-                        delete pg;
-                    }
-                    break;
-                case RORC_CFG_PROJECT_hlt_out:
-                    {
-                        librorc::patterngenerator *pg =
-                            new librorc::patterngenerator(link);
-                        pg->disable();
-                        delete pg;
-                    }
-                    break;
-                case RORC_CFG_PROJECT_hlt_in_fcf:
-                    {
-                        // DIU only available on lower 6 channels
-                        if ( i<6 )
+                        if( firmware_type == RORC_CFG_PROJECT_hlt_in_fcf )
                         {
-                            librorc::diu *diu = new librorc::diu(link);
-                            diu->disableInterface();
-                            diu->clearAllLastStatusWords();
-                            delete diu;
-
-                            link->disableDdr3DataReplayChannel();
-                            bar->set32(RORC_REG_DATA_REPLAY_CTRL, 0);
-                            link->disableFcf();
-                        } else {
-                            // TODO: disable raw readout
-                            librorc::diu *diu = new librorc::diu(link);
-                            diu->disableInterface();
-                            delete diu;
+                            librorc::fastclusterfinder *fcf = 
+                                new librorc::fastclusterfinder(link);
+                            fcf->setState(1, 0); // reset, not enabled
+                            delete fcf;
                         }
                     }
                     break;
-                default:
+
+                case RORC_CFG_LINK_TYPE_SIU:
+                    {
+                        librorc::siu *siu = new librorc::siu(link);
+                        siu->setReset(1);
+                        siu->disableInterface();
+                        siu->clearEventcount();
+                        siu->clearDeadtime();
+                        delete siu;
+                    }
                     break;
+
+                case RORC_CFG_LINK_TYPE_VIRTUAL:
+                    {
+                        librorc::ddl *ddlraw = new librorc::ddl(link);
+                        ddlraw->disableInterface();
+                        ddlraw->clearDeadtime();
+                        delete ddlraw;
+                    }
+                    break;
+
+                default: // LINK_TEST, IBERT
+                    {
+                    }
+                    break;
+            }
+
+            // PG on HLT_IN, HLT_OUT, HWTEST
+            if( link->patternGeneratorAvailable() )
+            {
+                librorc::patterngenerator *pg =
+                    new librorc::patterngenerator(link);
+                pg->disable();
+                delete pg;
+            }
+
+            // reset DDR3 Data Replay if available
+            if( link->ddr3DataReplayAvailable() )
+            {
+                link->setDdr3DataReplayChannelReset(1);
             }
         }
 

commit cd7ae4cfd14c820d773540fc418939434bf296cc

bugfix on link::isGtxDomainReady(), fixing regression from revision 25c9292

include/librorc/link.hh
@@ -142,6 +142,12 @@ namespace LIBRARY_NAME
             bool isGtxDomainReady();
 
             /**
+             * get GTX Link Status
+             * @return TRUE if up and running, FALSE if down
+             * */
+            bool isGtxLinkUp();
+
+            /**
              * wait for GTX domain to be ready read asynchronous GTX status
              * wait for rxresetdone & txresetdone & rxplllkdet & txplllkdet
              * & !gtx_in_rst

src/link.cpp
@@ -192,6 +192,28 @@ namespace LIBRARY_NAME
     bool
     link::isGtxDomainReady()
     {
+        uint32_t gtxasynccfg = packetizer(RORC_REG_GTX_ASYNC_CFG);
+        uint32_t stsmask = (1<<0) | // GTX reset
+            (1<<1) | // RX Reset
+            (1<<2) | // RX Reset Done
+            (1<<3) | // TX Reset
+            (1<<4) | // TX Reset Done
+            (1<<5) | // RX PLL LKDet
+            (1<<8); // GTX in Reset
+        uint32_t expected_value = (0<<0) | // no GTX reset
+            (0<<1) | // no RX Reset
+            (1<<2) | // RX Reset Done==1
+            (0<<3) | // no RX Reset
+            (1<<4) | // RX Reset Done==1
+            (1<<5) | // RX PLL LKDet
+            (0<<8); // GTX not in Reset
+        return ((gtxasynccfg & stsmask) == expected_value);
+    }
+
+
+    bool
+    link::isGtxLinkUp()
+    {
         uint32_t gtxsyncsts = GTX(RORC_REG_GTX_CTRL);
         uint32_t stsmask = (1<<0) | // PHY is ready
             (1<<1) | // PHY is enabled enable

tools_src/dma/dma_in_fcf.cpp
@@ -34,7 +34,8 @@ using namespace std;
 
 #define MAX_CHANNELS 12
 #define MAPPING_FILE "/build/ddl_data/FCF_Mapping/FCF_Mapping_Patch0.data"
-#define INPUT_FILE "/build/ddl_data/raw28/TPC_962.ddl"
+//#define INPUT_FILE "/build/ddl_data/raw28/TPC_962.ddl"
+#define INPUT_FILE "/build/ddl_data/raw28/TPC_768.ddl"
 #define INPUT_FILE2 "/build/ddl_data/raw28/TPC_959.ddl"
 
 bool done = false;
@@ -103,9 +104,21 @@ uint32_t fileToRam
 int main(int argc, char *argv[])
 {
     char logdirectory[] = "/tmp";
-    DMAOptions opts;
-    opts = evaluateArguments(argc, argv);
-    opts.esType = LIBRORC_ES_TO_HOST;
+    DMAOptions opts = evaluateArguments(argc, argv);
+
+    if
+    (!(
+        checkDeviceID(opts.deviceId, argv[0])    &&
+        checkChannelID(opts.channelId, argv[0])
+    ) )
+    { exit(-1); }
+
+    if
+    (
+        (opts.datasource == ES_SRC_HWPG) &&
+        !checkEventSize(opts.eventSize, argv[0])
+    )
+    { exit(-1); }
 
     DMA_ABORT_HANDLER_REGISTER
 
@@ -139,12 +152,6 @@ int main(int argc, char *argv[])
     cout << "Waiting for GTX domain..." << endl;
     link->waitForGTXDomain();
 
-#ifndef SIM
-    /** in simulation FCF RAM is loaded by Modelsim */
-    //cout << "Writing mapping file to FCF_RAM..." << endl;
-    //link->fcfLoadMappingRam(MAPPING_FILE);
-#endif
-
     /** wait until DDL is up */
     /*cout << "Waiting for DIU riLD_N..." << endl;
     while( (link->GTX(RORC_REG_DDL_CTRL) & 0x20) != 0x20)
@@ -164,26 +171,9 @@ int main(int argc, char *argv[])
         abort();
     }
 
-    //-----------------------------------------------------//
-
     /**
-     * Option1:
-     * configure datastream: from DIU through FCF
-     **/
-    /*link->setDataSourceDdl();
-    link->enableDdl();
-    link->enableFcf();
-    link->prepareDiuForFeeData(); */
-
-    //-----------------------------------------------------//
-
-    /**
-     * Option 2:
      * configure datastream: from DDR3 through FCF
      **/
-    //link->setDataSourceDdr3DataReplay();
-    //link->enableFcf();
-    //link->enableDdl();
 
     // disable Data Replay
     link->setDdr3DataReplayChannelReset(1);
@@ -196,8 +186,8 @@ int main(int argc, char *argv[])
     uint32_t ch_start_addr = 0;
     uint32_t next_addr = fileToRam(sm, INPUT_FILE, opts.channelId,
             ch_start_addr, false);
-    next_addr = fileToRam(sm, INPUT_FILE2, opts.channelId,
-            next_addr, true);
+    //next_addr = fileToRam(sm, INPUT_FILE2, opts.channelId,
+    //        next_addr, true);
 
     //-----------------------------------------------------//
 
@@ -207,8 +197,11 @@ int main(int argc, char *argv[])
 
     librorc::event_stream *eventStream;
     cout << "Prepare ES" << endl;
-    if( !(eventStream = prepareEventStream(dev, bar, opts)) )
+
+    eventStream = prepareEventStream(dev, bar, opts);
+    if( !eventStream )
     { exit(-1); }
+
     eventStream->setEventCallback(event_callback);
 
     last_bytes_received = 0;
@@ -216,9 +209,30 @@ int main(int argc, char *argv[])
 
     eventStream->printDeviceStatus();
 
-    /** enable EBDM + RBDM + PKT */
-    //link->setPacketizer(RORC_REG_DMA_CTRL,
-    //        (link->packetizer(RORC_REG_DMA_CTRL) | 0x0d) );
+
+    librorc::fastclusterfinder *fcf =
+        eventStream->getFastClusterFinder();
+    if( !fcf )
+    {
+        cout << "Clusterfinder not available for this FW/Channel!"
+             << endl;
+        abort();
+    }
+
+    fcf->setState(1, 0); // reset, not enabled
+#ifndef SIM
+    fcf->loadMappingRam(opts.fcfmappingname);
+#endif
+    fcf->setSinglePadSuppression(0);
+    fcf->setBypassMerger(0);
+    fcf->setDeconvPad(1);
+    fcf->setSingleSeqLimit(0);
+    fcf->setClusterLowerLimit(10);
+    fcf->setMergerDistance(4);
+    fcf->setMergerAlgorithm(1);
+    fcf->setChargeTolerance(0);
+    fcf->setState(0, 1);// not reset, enabled
+    delete fcf;
 
     link->setDataSourceDdr3DataReplay();
     link->setFlowControlEnable(1);
@@ -228,19 +242,16 @@ int main(int argc, char *argv[])
     link->setDdr3DataReplayChannelStartAddress(ch_start_addr);
     link->enableDdr3DataReplayChannel();
 
-
     /** make clear what will be checked*/
-    int32_t sanity_check_mask = CHK_SIZES|CHK_SOE|CHK_EOE;
+    //int32_t sanity_check_mask = CHK_SIZES|CHK_SOE|CHK_EOE;
+    int32_t sanity_check_mask = CHK_SIZES|CHK_EOE;
     if(opts.useRefFile)
-    {
-        sanity_check_mask |= CHK_FILE;
-    }
-    else
-    {
-        sanity_check_mask |= CHK_PATTERN | CHK_ID;
-    }
+    { sanity_check_mask |= CHK_FILE; }
+    else if ( opts.datasource == ES_SRC_HWPG )
+    { sanity_check_mask |= (CHK_PATTERN|CHK_ID|CHK_SOE); }
+    else if ( opts.datasource == ES_SRC_DIU)
+    { sanity_check_mask |= (CHK_DIU_ERR); }
 
-    cout << "Event Loop Start" << endl;
 
     librorc::event_sanity_checker checker =
         (opts.useRefFile)