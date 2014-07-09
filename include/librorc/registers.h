heikoengel/alice-hlt-librorc

commit 229c97cf8aa55bfc63ad3f7fb8fa54057b2a7938

fixing regression from dd05b1d in flisock
buffer: removing unused dma_direction, added support for phys->offset and offset->phys lookup

link method to check DMA engine state, used in dma_channel, fixed polarity (enable is active low)

src/link.cpp
@@ -541,13 +541,13 @@ namespace LIBRARY_NAME
     void
     link::disableDmaEngine()
     {
-        /**
-         * disable EBDM + RBDM
-         * disable Packetizer
-         * Reset D_FIFO
-         * Reset MaxPayload
-         **/
         setPciReg(RORC_REG_DMA_CTRL, 0X00000002);
+    }
+
+    bool
+    link::dmaEngineIsActive()
+    {
+        return ((pciReg(RORC_REG_DMA_CTRL) & 0x1)==0x0);
     }
 
 

fli/flisock.c
@@ -20,7 +20,7 @@
 // 0.2       Heiko Engel     22 Sep 2011   Adjusted for CRORC, Modelsim SE 10.0c_1
 //==============================================================================
 
-#ifdef SIM
+#ifdef MODELSIM
 
 #include <stdio.h>
 #include <math.h>

include/librorc/buffer.hh
@@ -99,8 +99,7 @@ class buffer_sglist_programmer;
                  device   *dev,
                  uint64_t  size,
                  uint64_t  id,
-                 int32_t   overmap,
-                 int32_t   dma_direction
+                 int32_t   overmap
              );
 
              /**
@@ -206,6 +205,33 @@ class buffer_sglist_programmer;
             { return( size()/sizeof(librorc_event_descriptor) ); }
 
 
+            /**
+             * resolv buffer offset to physical DMA address
+             * @param offset buffer offset to be resolved
+             * @param phys_addr pointer to uint64_t to save the physical address
+             * @return true on success, false if the offset was not found in the buffer
+             **/
+            bool
+            offsetToPhysAddr
+            (
+                uint64_t offset,
+                uint64_t *phys_addr
+            );
+
+            /**
+             * resolv a physical DMA address to buffer offset
+             * @param phys_addr physical address to be resolved
+             * @param offset pointer to uint64_t to save the buffer offset
+             * @return true on success, false if the physical address was not
+             * found in the buffer
+             **/
+            bool
+            physAddrToOffset
+            (
+                uint64_t phys_addr,
+                uint64_t *offset
+            );
+
         /**
          * @internal
          */
@@ -219,7 +245,6 @@ class buffer_sglist_programmer;
 
             uint32_t         *m_mem;
             uint64_t          m_id;
-            int32_t           m_dmaDirection;
             uint64_t          m_numberOfScatterGatherEntries;
             uint64_t          m_size;
 

include/librorc/link.hh
@@ -218,8 +218,16 @@ namespace LIBRARY_NAME
              **/
             uint64_t getRBSize();
 
-
+            /**
+             * kill DMA engine.
+             **/
             void disableDmaEngine();
+
+            /**
+             * check if DMA engine is running.
+             * @return true if running, false if stopped
+             **/
+            bool dmaEngineIsActive();
 
 
 

src/buffer.cpp
@@ -36,15 +36,20 @@ buffer::buffer
     device   *dev,
     uint64_t  size,
     uint64_t  id,
-    int32_t   overmap,
-    int32_t   dma_direction
+    int32_t   overmap
 )
 {
-    m_dmaDirection      = dma_direction;
     m_device            = dev->getPdaPciDevice();
 
     bool newAllocNeeded = false;
     m_buffer            = NULL;
+
+    if( size == 0 )
+    {
+        DEBUG_PRINTF(PDADEBUG_ERROR, "Requested buffer of size 0");
+        throw LIBRORC_BUFFER_ERROR_CONSTRUCTOR_FAILED;
+    }
+
     if(PDA_SUCCESS != PciDevice_getDMABuffer(m_device, id, &m_buffer) )
     { newAllocNeeded = true; }
     else
@@ -191,5 +196,49 @@ buffer::deallocate()
 }
 
 
+bool
+buffer::offsetToPhysAddr
+(
+    uint64_t offset,
+    uint64_t *phys_addr
+)
+{
+    std::vector<librorc_sg_entry>::iterator iter;
+    uint64_t reference_offset = 0;
+    for( iter=m_sglist_vector.begin(); iter!=m_sglist_vector.end(); iter++ )
+    {
+        if( offset>=reference_offset && offset<(reference_offset+iter->length) )
+        {
+            *phys_addr = iter->pointer + (offset - reference_offset);
+            return true;
+        }
+        else
+        { reference_offset += iter->length; }
+    }
+    return false;
+}
+
+
+bool
+buffer::physAddrToOffset
+(
+    uint64_t phys_addr,
+    uint64_t *offset
+)
+{
+    std::vector<librorc_sg_entry>::iterator iter;
+    uint64_t reference_offset = 0;
+    for( iter=m_sglist_vector.begin(); iter!=m_sglist_vector.end(); iter++ )
+    {
+        if( phys_addr>=iter->pointer && phys_addr<(iter->pointer+iter->length) )
+        {
+            *offset = reference_offset + (phys_addr - iter->pointer);
+            return true;
+        }
+        else
+        { reference_offset += iter->length; }
+    }
+    return false;
+}
 
 }

src/dma_channel.cpp
@@ -412,7 +412,7 @@ void
 dma_channel::disable()
 {
     // suspend DMA transfer only if active
-    if( DMAConfig() & 0x01 )
+    if( m_link->dmaEngineIsActive() )
     {
         setSuspend(1);
 

commit 5fc745fa1afdbb3aeb010d43a6b8fcde6b42042a

Bracket LED control support and integration into rorctl
fixing regression from 229c97c on dmaEngineIsActive polarity

include/librorc/registers.h
@@ -4,12 +4,12 @@
 // Note:
 // This file was automatically generated from:
 // src/packages/rorc_config.vhd
-// hash: 22a59f899bd49c2633bc580cc787b367dbafc0ad
-// short hash: 22a59f8
+// hash: d3a99c299630c28df6f415e0cffa9ec4c18dc88a
+// short hash: d3a99c2
 //
 
-#define RORC_REVISION 0x22a59f8
-#define RORC_BUILD_DATE 0x20140527
+#define RORC_REVISION 0xd3a99c2
+#define RORC_BUILD_DATE 0x20140624
 
 #define RORC_CHANNEL_OFFSET 0x00008000
 #define RORC_REG_FIRMWARE_REVISION 0
@@ -28,6 +28,7 @@
 #define RORC_REG_PCIE_CTRL 16
 #define RORC_REG_TYPE_CHANNELS 17
 #define RORC_REG_PCIE_DST_BUSY 18
+#define RORC_REG_BRACKET_LED_CTRL 19
 #define RORC_REG_FAN_CTRL 20
 #define RORC_REG_PCIE_TERR_DROP 21
 #define RORC_REG_UC_SPI_DATA 22

src/link.cpp
@@ -547,7 +547,7 @@ namespace LIBRARY_NAME
     bool
     link::dmaEngineIsActive()
     {
-        return ((pciReg(RORC_REG_DMA_CTRL) & 0x1)==0x0);
+        return ((pciReg(RORC_REG_DMA_CTRL) & 0x1)==0x1);
     }
 
 

tools_src/rorctl/rorctl.cpp
@@ -62,6 +62,7 @@ Instruction parameters :                                     \n\
   -s              Show flash status                          \n\
   -r              Reset Flash                                \n\
   -m              Show CRORC monitor stats                   \n\
+  -b [0,1]        Set CRORC bracket LED blinking on/off      \n\
 Examples :                                                   \n\
 Show status of Device 0 Flash 0:                             \n\
   rorctl -n 0 -c 0 -s                                        \n\
@@ -96,6 +97,13 @@ void
 print_device
 (
     uint8_t index
+);
+
+void
+set_led_blink_mode
+(
+    uint8_t index,
+    uint32_t mode
 );
 
 inline
@@ -149,7 +157,7 @@ int main
     {
         opterr = 0;
         int c;
-        while((c = getopt(argc, argv, "hvlmdepn:f:c:sr")) != -1)
+        while((c = getopt(argc, argv, "hvlmdepn:f:c:srb:")) != -1)
         {
             switch(c)
             {
@@ -269,6 +277,12 @@ int main
                 }
                 break;
 
+                case 'b':
+                {
+                    set_led_blink_mode(options.device_number, atoi(optarg));
+                }
+                break;
+
                 default:
                 {
                     cout << "Unknown parameter (" << c << ")!" << endl;
@@ -305,6 +319,38 @@ print_devices()
     {
         print_device(i);
     }
+}
+
+void
+set_led_blink_mode(
+    uint8_t index,
+    uint32_t mode
+)
+{
+    /** Instantiate device with index <index> */
+    librorc::device *dev = NULL;
+    try{ dev = new librorc::device(index);}
+    catch(...){ exit(0); }
+
+    /** Instantiate a new bar */
+    librorc::bar *bar = NULL;
+    try
+    {
+    #ifdef SIM
+        bar = new librorc::sim_bar(dev, 1);
+    #else
+        bar = new librorc::rorc_bar(dev, 1);
+    #endif
+    }
+    catch(...)
+    {
+        printf("ERROR: failed to initialize BAR.\n");
+        return;
+    }
+
+    bar->set32(RORC_REG_BRACKET_LED_CTRL, mode<<31);
+    delete bar;
+    delete dev;
 }
 
 