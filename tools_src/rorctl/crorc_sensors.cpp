8038aa39c2dbfd23775cb27b1ebdf98fba6e822a
Heiko Engel <hengel@cern.ch>
2014-08-29 10:42:33 +0200

    continuing crorc_sensors - adding some metrics, support to print all metrics at once

--- tools_src/rorctl/crorc_sensors.cpp
+++ tools_src/rorctl/crorc_sensors.cpp
@@ -31,6 +31,7 @@
   "  -d|--device [n]  select target C-RORC device. Uses device 0 if not\n"     \
   "                   specified\n"                                             \
   "  -l|--list        list all available sensors\n"                            \
+  "  -a|--all         print all sensor values\n"                               \
   "  --fpga_temp      print FPGA Temperature in Degree Celsius\n"              \
   "  --fpga_fw_rev    print FPGA Firmware Revision\n"                          \
   "  --fpga_fw_date   print FPGA Firmware Date\n"                              \
@@ -38,17 +39,35 @@
   "  --fpga_fw_descr  print Firmware Description string\n"                     \
   "  --fpga_vcc_int   print FPGA Core Voltage in V\n"                          \
   "  --fpga_vcc_aux   print FPGA Auxilliary Voltage in V\n"                    \
-  "  --fan_speed       print FPGA Fan RPM or 0 if stopped\n"                    \
+  "  --fan_speed       print FPGA Fan RPM or 0 if stopped\n"                   \
   "  --pcie_slot      print PCIe Domain:Bus:Slot.Funtion numbers\n"            \
   "  --pcie_lanes     print number of PCIe lanes used for C-RORC\n"            \
   "  --pcie_gen       print PCIe Generation used for C-RORC\n"                 \
   "  --pcie_tx_error  print PCIe TX Error Count\n"                             \
+  "  --pcie_ill_req   print PCIe Illegal Request Count\n"                      \
   "  --smbus_slv_addr print SMBus Slave Address for Configuration\n"           \
   "                   Controller access\n"                                     \
   "\n"
 
+const char *composeFormat(const char *name, const char *fmt, int withName) {
+  std::string full_fmt = "";
+  if (withName) {
+    full_fmt += "%s: ";
+  }
+  full_fmt += std::string(fmt) + "\n";
+  return full_fmt.c_str();
+}
+
+#define PRINT_METRIC(name, fmt, value, withName)                               \
+  const char *full_fmt = composeFormat(name, fmt, withName);                   \
+  if (withName) {                                                              \
+    printf(full_fmt, name, value);                                             \
+  } else {                                                                     \
+    printf(full_fmt, value);                                                   \
+  }
+
 int main(int argc, char *argv[]) {
-  //int sAll = 0;
+  int sAll = 0;
   int sFpgaTemp = 0;
   int sFpgaFwDate = 0;
   int sFpgaFwRev = 0;
@@ -56,18 +75,20 @@ int main(int argc, char *argv[]) {
   int sFpgaFwDesrc = 0;
   int sFpgaVccInt = 0;
   int sFpgaVccAux = 0;
+  int sFpgaSysclkActive = 0;
   int sFpgaFan = 0;
   int sPcieSlot = 0;
   int sPcieLanes = 0;
   int sPcieGen = 0;
   int sPcieTxErr = 0;
+  int sPcieIllReq = 0;
   int sSmbusSlvAddr = 0;
+  int sNumQsfps = 0;
   uint32_t deviceId = 0;
 
   static struct option long_options[] = {
-    { "help", no_argument, 0, 'h' },
-    { "device", required_argument, 0, 'd' },
-    { "list", no_argument, 0, 'l' },
+    { "help", no_argument, 0, 'h' }, { "device", required_argument, 0, 'd' },
+    { "list", no_argument, 0, 'l' }, { "all", no_argument, 0, 'a' },
     { "fpga_temp", no_argument, &sFpgaTemp, 1 },
     { "fpga_fw_rev", no_argument, &sFpgaFwRev, 1 },
     { "fpga_fw_date", no_argument, &sFpgaFwDate, 1 },
@@ -75,19 +96,21 @@ int main(int argc, char *argv[]) {
     { "fpga_fw_descr", no_argument, &sFpgaFwDesrc, 1 },
     { "fpga_vcc_int", no_argument, &sFpgaVccInt, 1 },
     { "fpga_vcc_aux", no_argument, &sFpgaVccAux, 1 },
+    { "fpga_sysclk_active", no_argument, &sFpgaSysclkActive, 1 },
     { "fan_speed", no_argument, &sFpgaFan, 1 },
     { "pcie_slot", no_argument, &sPcieSlot, 1 },
     { "pcie_lanes", no_argument, &sPcieLanes, 1 },
     { "pcie_gen", no_argument, &sPcieGen, 1 },
     { "pcie_tx_error", no_argument, &sPcieTxErr, 1 },
+    { "pcie_ill_req", no_argument, &sPcieIllReq, 1 },
     { "smbus_slv_addr", no_argument, &sSmbusSlvAddr, 1 },
-    { 0, 0, 0, 0 }
+    { "num_qsfps", no_argument, &sNumQsfps, 1 }, { 0, 0, 0, 0 }
   };
 
   /** Parse command line arguments **/
   if (argc > 1) {
     while (1) {
-      int opt = getopt_long(argc, argv, "hld:", long_options, NULL);
+      int opt = getopt_long(argc, argv, "hld:a", long_options, NULL);
       if (opt == -1) {
         break;
       }
@@ -98,6 +121,9 @@ int main(int argc, char *argv[]) {
         return -1;
       case 'd':
         deviceId = strtol(optarg, NULL, 0);
+        break;
+      case 'a':
+        sAll = 1;
         break;
       case 'l': {
         int iter = 0;
@@ -152,51 +178,74 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  if (sFpgaTemp) {
-    printf("%.1f\n", sm->FPGATemperature());
+  if (sFpgaTemp || sAll) {
+    PRINT_METRIC("fpga_temp", "%d", (uint32_t) sm->FPGATemperature(), sAll);
   }
-  if (sFpgaFwRev) {
-    printf("0x%08x\n", sm->FwRevision());
+  if (sFpgaFwRev || sAll) {
+    PRINT_METRIC("fpga_fw_rev", "0x%08x", sm->FwRevision(), sAll);
   }
-  if (sFpgaFwDate) {
-    printf("%08x\n", sm->FwBuildDate());
+  if (sFpgaFwDate || sAll) {
+    PRINT_METRIC("fpga_fw_date", "%08x", sm->FwBuildDate(), sAll);
   }
-  if (sFpgaUptime) {
-    printf("%ld\n", sm->uptimeSeconds());
+  if (sFpgaUptime || sAll) {
+    PRINT_METRIC("fpga_uptime", "%ld", sm->uptimeSeconds(), sAll);
   }
-  if (sFpgaFwDesrc) {
-    printf("%s\n", sm->firmwareDescription());
+  if (sFpgaFwDesrc || sAll) {
+    PRINT_METRIC("fpga_fw_descr", "%s", sm->firmwareDescription(), sAll);
   }
-  if (sFpgaVccInt) {
-      printf("%.2f\n", sm->VCCINT());
+  if (sFpgaVccInt || sAll) {
+    PRINT_METRIC("fpga_vcc_int", "%.2f", sm->VCCINT(), sAll);
   }
-  if (sFpgaVccAux) {
-      printf("%.2f\n", sm->VCCAUX());
+  if (sFpgaVccAux || sAll) {
+    PRINT_METRIC("fpga_vcc_aux", "%.2f", sm->VCCAUX(), sAll);
   }
-  if (sFpgaFan) {
-    if (!sm->systemFanIsRunning()) {
-      printf("0\n");
-    } else {
-      printf("%d\n", (int)(sm->systemFanSpeed()));
-    }
+  if (sFpgaSysclkActive || sAll) {
+    PRINT_METRIC("fpga_sysclk_active", "%d",
+                 (uint32_t) sm->systemClockIsRunning(), sAll);
   }
-  if (sPcieSlot) {
-    printf("%04d:%02d:%02d.%d\n", dev->getDomain(), dev->getBus(),
-           dev->getSlot(), dev->getFunc());
+  if (sFpgaFan || sAll) {
+    uint32_t fan_speed =
+        (sm->systemFanIsRunning()) ? (uint32_t) sm->systemFanSpeed() : 0;
+    PRINT_METRIC("fan_speed", "%d", fan_speed, sAll);
   }
-  if (sPcieLanes) {
-    printf("%d\n", sm->pcieNumberOfLanes());
-  }
-  if (sPcieGen) {
-    printf("%d\n", sm->pcieGeneration());
-  }
-  if (sSmbusSlvAddr) {
-    printf("0x%02x\n", sm->dipswitch());
-  }
-  if (sPcieTxErr) {
-      printf("%d\n", sm->pcieTransmissionErrorCounter());
-  }
+  if (sPcieSlot || sAll) {
+    uint32_t domain = (uint32_t) dev->getDomain();
+    uint32_t bus = (uint32_t) dev->getBus();
+    uint32_t slot = (uint32_t) dev->getSlot();
+    uint32_t func = (uint32_t) dev->getFunc();
 
+    std::stringstream ss;
+    ss << std::hex << std::setfill('0') << std::setw(4) << domain << ":"
+       << std::setw(2) << bus << ":" << std::setw(2) << slot << "."
+       << std::setw(1) << func;
+    std::string pcie_slot = ss.str();
+    PRINT_METRIC("pcie_slot", "%s", pcie_slot.c_str(), sAll);
+  }
+  if (sPcieLanes || sAll) {
+    PRINT_METRIC("pcie_lanes", "%d", sm->pcieNumberOfLanes(), sAll);
+  }
+  if (sPcieGen || sAll) {
+    PRINT_METRIC("pcie_gen", "%d", sm->pcieGeneration(), sAll);
+  }
+  if (sSmbusSlvAddr || sAll) {
+    PRINT_METRIC("smbus_slv_addr", "0x%02x", sm->dipswitch(), sAll);
+  }
+  if (sPcieTxErr || sAll) {
+    PRINT_METRIC("pcie_tx_error", "%d", sm->pcieTransmissionErrorCounter(),
+                 sAll);
+  }
+  if (sPcieIllReq || sAll) {
+    PRINT_METRIC("pcie_ill_req", "%d", sm->pcieIllegalRequestCounter(), sAll);
+  }
+  if (sNumQsfps || sAll) {
+    uint32_t num_qsfps = 0;
+    for (int i = 0; i < LIBRORC_MAX_QSFP; i++) {
+      if (sm->qsfpIsPresent(i)) {
+        num_qsfps++;
+      }
+    }
+    PRINT_METRIC("num_qsfps", "%d", num_qsfps, sAll);
+  }
 
   if (sm) {
     delete sm;
